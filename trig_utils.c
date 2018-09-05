17e55c0466dd823ba4f7ef759640e1e89f3f6455
Martin Reinecke <martin@mpa-garching.mpg.de>
2018-09-05 11:01:34 +0200

    remove unneeded code

--- trig_utils.c
+++ trig_utils.c
@@ -1,30 +1,6 @@
-/*
- *  This file is part of libc_utils.
- *
- *  libc_utils is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  libc_utils is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with libc_utils; if not, write to the Free Software
- *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- */
-
-/*
- *  libc_utils is being developed at the Max-Planck-Institut fuer Astrophysik
- *  and financially supported by the Deutsches Zentrum fuer Luft- und Raumfahrt
- *  (DLR).
- */
-
 /*! \file trig_utils.c
  *
- *  Copyright (C) 2016-2017 Max-Planck-Society
+ *  Copyright (C) 2016-2018 Max-Planck-Society
  *  \author Martin Reinecke
  *
  *  Many inspirations for this code come from Tasche and Zeuner: "Improved
@@ -59,35 +35,6 @@ void fracsincos(int m, int n, double *s, double *c)
   if (octant & 1) { double t = *c; *c = *s; *s = t; }
   if (octant & 2) { double t = *c; *c = -(*s); *s = t; }
   if (octant & 4) { *s = -(*s); }
-  }
-
-void sincos_multi (size_t n, double alpha, double *s, double *c,
-  int stride)
-  {
-  if (n==0) return;
-  s[0] = 0.; c[0]=1.;
-  if (n==1) return;
-  size_t l1=(size_t)sqrt(n);
-  double scur=0.,ccur=1.;
-  for (size_t i=1,m=0,a=1; i<n; ++i,++a)
-    {
-    if (a==l1)
-      {
-      a=0;
-      ++m;
-      scur=sin(i*alpha); ccur=cos(i*alpha);
-      }
-    if (m==0)
-      {
-      s[i*stride]=sin(i*alpha);
-      c[i*stride]=cos(i*alpha);
-      }
-    else
-      {
-      c[i*stride]=ccur*c[a*stride] - scur*s[a*stride];
-      s[i*stride]=ccur*s[a*stride] + scur*c[a*stride];
-      }
-    }
   }
 
 static void fracsincos_multi_priv (size_t n, int num, int den, double *s,
@@ -127,10 +74,6 @@ static void fracsincos_multi_priv (size_t n, int num, int den, double *s,
       }
     }
   }
-
-void fracsincos_multi (size_t n, int num, int den, double *s, double *c,
-  int stride)
-  { fracsincos_multi_priv (n,num,den,s,c,stride,0); }
 
 static void sincos_2pibyn_priv (size_t n, size_t nang, double *s, double *c,
   int stride, int exact)
